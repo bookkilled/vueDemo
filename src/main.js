4d31dc4b85ca71d329fe6119205b03b28718bc85
bookkilled <bookkilled@gmail.com>
2019-09-18 17:31:27 +0800

    input复制

--- src/main.js
+++ src/main.js
@@ -2,6 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Directives from './directives'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+import '@vant/touch-emulator'
+
+Vue.use(Vant)
+
+Vue.use(Directives)
 
 Vue.config.productionTip = false
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="home">
+		<van-row class="maxW">
+			<van-col span="8">复制v-copy</van-col>
+			<van-col span="8"><van-field v-model="textinp" placeholder="请输入用户名" /></van-col>
+			<van-col span="8"><van-button type="primary" class="btn" v-copy="textinp">copy</van-button></van-col>
+		</van-row>
+		<!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+		<!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+	</div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+	name: 'home',
+	data () {
+		return {
+			textinp: ''
+		}
+	},
+	components: {
+		// HelloWorld
+	},
+	methods: {
+		copyInp () {
+			console.log(this.textinp)
+		}
+	}
 }
 </script>
+<style lang="less" scoped>
+.maxW {
+	width: 50%;
+	margin: 0 auto;
+	line-height: 44px;
+}
+</style>
